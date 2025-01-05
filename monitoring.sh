e9cae1033aafede4d0a8429fadef15d53a2c1b0e
dsemenov <extraden7@gmail.com>
2025-01-05 11:30:36 -0600

    add RAM checking

--- monitoring.sh
+++ monitoring.sh
@@ -4,13 +4,18 @@
 arch=$(uname -a)
 
 # Number of physical processors (sockets)
-physical_processors=$(lscpu | grep 'Socket' | awk '{print $2}')
+physical_processors=$(lscpu | grep '^Socket(s):' | awk '{print $2}')
 
 # Number of virtual processors
 virtual_processors=$(nproc)
 
+# RAM usage
+used_ram=$(free -m | grep '^Mem:' | awk '{print $3}')
+total_ram=$(free -m | grep '^Mem:' | awk '{print $2}')
+used_ram_percentage=$((used_ram * 100 / total_ram))
 wall "
 #Architecture: $arch
-#Number of physical processors: $physical_processors
-#Number of virtual processors: $virtual_processors
+#CPU physical: $physical_processors
+#vCPU: $virtual_processors
+#Memory Usage: $used_ram MB / $total_ram MB ($used_ram_percentage%)
 "
