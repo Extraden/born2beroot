f532a06fafb37ef6ef8379911da8b76a689cfd00
dsemenov <extraden7@gmail.com>
2025-01-05 13:46:05 -0600

    add CPU usage

--- monitoring.sh
+++ monitoring.sh
@@ -18,10 +18,15 @@ used_ram_percentage=$((used_ram * 100 / total_ram))
 disk_used=$(df -m --total | awk '/^total/ {print $3}')
 total_disk=$(df -m --total | awk '/^total/ {print $2}')
 disk_usage_percentage=$((disk_used * 100 / total_disk))
+
+# CPU usage
+cpu_usage=$(top -bn 1 | awk -F ',' '/^%Cpu\(s\)/ {print $4}' | awk '{print 100 - $1}')
+
 wall "
 #Architecture: $arch
 #CPU physical: $physical_processors
 #vCPU: $virtual_processors
 #Memory Usage: $used_ram MB / $total_ram MB ($used_ram_percentage%)
 #Disk Usage: $disk_used MB / $total_disk MB ($disk_usage_percentage%)
+#CPU load: $cpu_usage
 "
