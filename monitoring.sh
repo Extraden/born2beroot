5b6c440da05c5be5e8d64d6400f9d0e444f1543a
dsemenov <extraden7@gmail.com>
2025-01-05 13:25:08 -0600

    Add disk usage

--- monitoring.sh
+++ monitoring.sh
@@ -13,9 +13,15 @@ virtual_processors=$(nproc)
 used_ram=$(free -m | grep '^Mem:' | awk '{print $3}')
 total_ram=$(free -m | grep '^Mem:' | awk '{print $2}')
 used_ram_percentage=$((used_ram * 100 / total_ram))
+
+# Disk usage
+disk_used=$(df -m --total | awk '/^total/ {print $3}')
+total_disk=$(df -m --total | awk '/^total/ {print $2}')
+disk_usage_percentage=$((disk_used * 100 / total_disk))
 wall "
 #Architecture: $arch
 #CPU physical: $physical_processors
 #vCPU: $virtual_processors
 #Memory Usage: $used_ram MB / $total_ram MB ($used_ram_percentage%)
+#Disk Usage: $disk_used MB / $total_disk MB ($disk_usage_percentage%)
 "
