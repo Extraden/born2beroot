819658ee1c64092e9a9d2b0e8c76940950bed3e3
dsemenov <extraden7@gmail.com>
2025-01-05 11:08:54 -0600

    add number of virtual processors. Fix typo

--- monitoring.sh
+++ monitoring.sh
@@ -6,7 +6,11 @@ arch=$(uname -a)
 # Number of physical processors (sockets)
 physical_processors=$(lscpu | grep 'Socket' | awk '{print $2}')
 
+# Number of virtual processors
+virtual_processors=$(nproc)
+
 wall "
 #Architecture: $arch
-#Number of psycical processors: $physical_processors
+#Number of physical processors: $physical_processors
+#Number of virtual processors: $virtual_processors
 "
